b010a7dd80f8bc82bd101c1dfd096c850a893050
Chaitanya Deorukhkar <deorukhkarchaitanya@gmail.com>
2018-03-24 19:15:41 +0530

    Add bottom navigation

--- views/EmployeeHome.js
+++ views/EmployeeHome.js
@@ -8,6 +8,8 @@ import {
   LayoutAnimation
 } from "react-native";
 import Btn from "react-native-micro-animated-button";
+import BottomNavigation, { Tab } from 'react-native-material-bottom-navigation'
+import Icon from 'react-native-vector-icons/MaterialIcons'
 const { width } = Dimensions.get("window");
 const { height } = Dimensions.get("window");
 
@@ -20,12 +22,37 @@ export default class EmployeeHome extends Component {
     };
   }
 
-  componentDidMount() {
-  }
+  componentDidMount() {}
 
   render() {
     return (
       <View style={styles.container}>
+        <BottomNavigation
+          labelColor="white"
+          rippleColor="white"
+          style={{
+            height: 56,
+            elevation: 8,
+            position: "absolute",
+            left: 0,
+            bottom: 0,
+            right: 0
+          }}
+          onTabChange={newTabIndex =>
+            alert(`New Tab at position ${newTabIndex}`)
+          }
+        >
+          <Tab
+            barBackgroundColor="#37474F"
+            label="Movies & TV"
+            icon={<Icon size={24} color="white" name="tv" />}
+          />
+          <Tab
+            barBackgroundColor="#00796B"
+            label="Music"
+            icon={<Icon size={24} color="white" name="music-note" />}
+          />
+        </BottomNavigation>
         <View
           style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
         >
