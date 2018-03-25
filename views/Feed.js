1d0d4243128721392bbb9ffd1ab36758c7e7116b
Chaitanya Deorukhkar <deorukhkarchaitanya@gmail.com>
2018-03-25 13:22:46 +0530

    Add feed updates

--- views/Feed.js
+++ views/Feed.js
@@ -7,11 +7,15 @@ import {
   Dimensions,
   LayoutAnimation,
   StatusBar,
-  StatusBarAnimation
+  ScrollView,
+  Image,
+  TouchableOpacity
 } from "react-native";
 import Btn from "react-native-micro-animated-button";
-import { Actions } from "react-native-router-flux";
-
+import { Actions, ActionConst } from "react-native-router-flux";
+import AnimatedBar from "react-native-animated-bar";
+import { List, ListItem, CardItem, Card, Thumbnail } from "native-base";
+import Icon from "react-native-vector-icons/MaterialIcons";
 const { width } = Dimensions.get("window");
 const { height } = Dimensions.get("window");
 
@@ -34,22 +38,131 @@ export default class Feed extends Component {
           barStyle="light-content"
           translucent={true}
           animated={true}
-          showHideTransition='slide'
+          showHideTransition="slide"
         />
-        <View
-          style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
+        <ScrollView
+          style={{
+            marginBottom: "13%",
+            padding: "2%",
+            flex: 1,
+            width: width,
+            marginTop: "5%"
+          }}
         >
-          <Text
-            style={{
-              color: "#3399CC",
-              fontWeight: "bold",
-              fontSize: 25,
-              paddingTop: "30%"
-            }}
-          >
-            Feed
-          </Text>
-        </View>
+          <View style={{ marginBottom: "5%" }}>
+            <Card>
+              <CardItem
+                stlye={{
+                  width: width,
+                  height: 50,
+                  padding: "5%",
+                  flex: 1
+                }}
+              >
+                <View style={{ flex: 1, paddingBottom: "3%" }}>
+                  <View
+                    style={{
+                      flex: 1,
+                      flexDirection: "column",
+                      width: "100%",
+                      height: 20
+                    }}
+                  />
+                  <View
+                    style={{
+                      flex: 1,
+                      flexDirection: "row",
+                      width: "100%",
+                      alignItems:'center'
+                    }}
+                  >
+                    <Thumbnail
+                      ref="thumbnail"
+                      size={40}
+                      source={require("../images/pv.jpg")}
+                    />
+                    <Text style={{paddingLeft:'2%'}}>Prasad Vagal completed the Java course</Text>
+                  </View>
+                  <Text style={{ fontSize: 10, paddingLeft:'15%' }}> 23-03-2018, 1:49 pm</Text>
+                </View>
+              </CardItem>
+            </Card>
+            <Card>
+              <CardItem
+                stlye={{
+                  width: width,
+                  height: 50,
+                  padding: "5%",
+                  flex: 1
+                }}
+              >
+                <View style={{ flex: 1, paddingBottom: "3%" }}>
+                  <View
+                    style={{
+                      flex: 1,
+                      flexDirection: "column",
+                      width: "100%",
+                      height: 20
+                    }}
+                  />
+                  <View
+                    style={{
+                      flex: 1,
+                      flexDirection: "row",
+                      width: "100%",
+                      alignItems:'center'
+                    }}
+                  >
+                    <Thumbnail
+                      ref="thumbnail"
+                      size={40}
+                      source={require("../images/empty.jpg")}
+                    />
+                    <Text style={{paddingLeft:'2%'}}>Sanjeet Mukadam completed the Kotlin course</Text>
+                  </View>
+                  <Text style={{ fontSize: 10, paddingLeft:'15%' }}> 22-03-2018, 05:19 pm</Text>
+                </View>
+              </CardItem>
+            </Card>
+            <Card>
+              <CardItem
+                stlye={{
+                  width: width,
+                  height: 50,
+                  padding: "5%",
+                  flex: 1
+                }}
+              >
+                <View style={{ flex: 1,}}>
+                  <View
+                    style={{
+                      flex: 1,
+                      flexDirection: "column",
+                      width: "100%",
+                      height: 20
+                    }}
+                  />
+                  <View
+                    style={{
+                      flex: 1,
+                      flexDirection: "row",
+                      width: "100%",
+                      alignItems:'center'
+                    }}
+                  >
+                    <Thumbnail
+                      ref="thumbnail"
+                      size={40}
+                      source={require("../images/empty.jpg")}
+                    />
+                    <Text style={{paddingLeft:'2%'}}>Chaitanya Deorukhkar completed the Java course</Text>
+                  </View>
+                  <Text style={{ fontSize: 10, paddingLeft:'15%' }}> 22-03-2018, 11:34 am</Text>
+                </View>
+              </CardItem>
+            </Card>
+          </View>
+        </ScrollView>
       </View>
     );
   }
