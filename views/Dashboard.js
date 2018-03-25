b9a8063bf425377c2f533153a9e027a3bf284590
Chaitanya Deorukhkar <deorukhkarchaitanya@gmail.com>
2018-03-25 12:07:11 +0530

    Add a profile picture

--- views/Dashboard.js
+++ views/Dashboard.js
@@ -12,7 +12,7 @@ import {
 import Btn from "react-native-micro-animated-button";
 import { Actions } from "react-native-router-flux";
 import AnimatedBar from "react-native-animated-bar";
-import { List, ListItem, CardItem, Card } from "native-base";
+import { List, ListItem, CardItem, Card, Thumbnail } from "native-base";
 import Icon from "react-native-vector-icons/MaterialIcons";
 
 const { width } = Dimensions.get("window");
@@ -43,107 +43,223 @@ export default class Dashboard extends Component {
           style={{
             flex: 1,
             width: width,
-            marginTop: "10%",
-            padding: "2%",
-            marginBottom: "10%"
           }}
         >
-        <View style={{width:width,alignItems:'center'}}>
-          <Text style={{fontSize:20,color:'#283593'}}>Prasad Vagal</Text>
-        </View>
-        <View style={{width:width,alignItems:'flex-start'}}>
-          <Text style={{fontSize:17}}>Skills acquired:</Text>
-        </View>
-        
-        <View style={{width:width,alignItems:'flex-start',height:40}}>
-        </View>
+          <View
+            style={{ width: width, alignItems: "center",height:'27%' }}
+          >
+            <Thumbnail
+              ref="thumbnail"
+              size={100}
+              source={require("../images/pv.jpg")}
+              style={{ transform: [{ scale: 2 }], marginTop: "15%" }}
+            />
+             <View
+            style={{ width: width, alignItems: "center", marginTop: "10%" }}
+          >
+            <Text style={{ fontSize: 20, color: "#283593" }}>Prasad Vagal</Text>
+          </View>
+          </View>
+
+         
+          <View style={{ width: width, alignItems: "flex-start", paddingLeft:'2%'}}>
+            <Text style={{ fontSize: 17 }}>Skills: </Text>
+          </View>
+          <View
+            style={{
+              width: width,
+              alignItems: "flex-start",
+              paddingLeft: "2%"
+            }}
+          >
+            <Text style={{ color: "black" }}>React JS, Android SDK, PHP</Text>
+          </View>
+          <View
+            style={{ width: width, alignItems: "flex-start", paddingTop: "2%",  paddingLeft:'2%' }}
+          >
+            <Text style={{ fontSize: 17 , }}>Skills acquired:</Text>
+          </View>
+          <View
+            style={{
+              width: width,
+              alignItems: "flex-start",
+              paddingLeft: "2%",
+              paddingBottom:'5%'
+            }}
+          >
+            <Text style={{ color: "black" }}>C++</Text>
+          </View>
           <ScrollView>
             <Text
               style={{
                 color: "#1c1c1c",
                 fontWeight: "bold",
                 fontSize: 20,
-                marginBottom:'2%'
+                marginBottom: "2%"
               }}
             >
               My courses:
             </Text>
             <Card>
-              <CardItem stlye={{
-                    width: width,
-                    height:50,
-                    padding: "5%",
-                    flex:1,
-                  }}>
-                  <View style={{flex:1,paddingBottom:'3%'}}>
-                  <View style={{flex:1,flexDirection:'column',width:'100%',height:20}}>
-                  <Text>Demo Course 1 (7 out of 10 complete)</Text>
+              <CardItem
+                stlye={{
+                  width: width,
+                  height: 50,
+                  padding: "5%",
+                  flex: 1
+                }}
+              >
+                <View style={{ flex: 1, paddingBottom: "3%" }}>
+                  <View
+                    style={{
+                      flex: 1,
+                      flexDirection: "column",
+                      width: "100%",
+                      height: 20
+                    }}
+                  >
+                    <Text>Java (7 out of 10 complete)</Text>
                   </View>
-                  <View style={{flex:1,flexDirection:'row',width:'100%',height:20}}>
-                  <View style={{ flex: 3, backgroundColor: "#4CAF50", }} />
-                  <View style={{ flex: 1, backgroundColor: "#E0E0E0" }} />
-                  <Icon size={24} color="black" name="keyboard-arrow-right" style={{marginLeft:'2%'}} />
+                  <View
+                    style={{
+                      flex: 1,
+                      flexDirection: "row",
+                      width: "100%",
+                      height: 20
+                    }}
+                  >
+                    <View style={{ flex: 3, backgroundColor: "#4CAF50" }} />
+                    <View style={{ flex: 1, backgroundColor: "#E0E0E0" }} />
+                    <Icon
+                      size={24}
+                      color="black"
+                      name="keyboard-arrow-right"
+                      style={{ marginLeft: "2%" }}
+                    />
                   </View>
-                  </View>
+                </View>
               </CardItem>
             </Card>
 
-             <Card>
-              <CardItem stlye={{
-                    width: width,
-                    height:50,
-                    padding: "5%",
-                    flex:1,
-                  }}>
-                  <View style={{flex:1,paddingBottom:'3%'}}>
-                  <View style={{flex:1,flexDirection:'column',width:'100%',height:20}}>
-                  <Text>Demo Course 2 (10 out of 10 complete)</Text>
+            <Card>
+              <CardItem
+                stlye={{
+                  width: width,
+                  height: 50,
+                  padding: "5%",
+                  flex: 1
+                }}
+              >
+                <View style={{ flex: 1, paddingBottom: "3%" }}>
+                  <View
+                    style={{
+                      flex: 1,
+                      flexDirection: "column",
+                      width: "100%",
+                      height: 20
+                    }}
+                  >
+                    <Text>C++ (10 out of 10 complete)</Text>
                   </View>
-                  <View style={{flex:1,flexDirection:'row',width:'100%',height:20}}>
-                  <View style={{ flex: 1, backgroundColor: "#8C9EFF", }} />
-                  <Icon size={24} color="royalblue" name="done" style={{marginLeft:'2%'}} />
+                  <View
+                    style={{
+                      flex: 1,
+                      flexDirection: "row",
+                      width: "100%",
+                      height: 20
+                    }}
+                  >
+                    <View style={{ flex: 1, backgroundColor: "#8C9EFF" }} />
+                    <Icon
+                      size={24}
+                      color="royalblue"
+                      name="done"
+                      style={{ marginLeft: "2%" }}
+                    />
                   </View>
-                  </View>
+                </View>
               </CardItem>
             </Card>
 
-             <Card>
-              <CardItem stlye={{
-                    width: width,
-                    height:50,
-                    padding: "5%",
-                    flex:1,
-                  }}>
-                  <View style={{flex:1,paddingBottom:'3%'}}>
-                  <View style={{flex:1,flexDirection:'column',width:'100%',height:20}}>
-                  <Text>Demo Course 3 (7 out of 10 complete)</Text>
+            <Card>
+              <CardItem
+                stlye={{
+                  width: width,
+                  height: 50,
+                  padding: "5%",
+                  flex: 1
+                }}
+              >
+                <View style={{ flex: 1, paddingBottom: "3%" }}>
+                  <View
+                    style={{
+                      flex: 1,
+                      flexDirection: "column",
+                      width: "100%",
+                      height: 20
+                    }}
+                  >
+                    <Text>Blockchain (7 out of 10 complete)</Text>
                   </View>
-                  <View style={{flex:1,flexDirection:'row',width:'100%',height:20}}>
-                  <View style={{ flex: 4, backgroundColor: "#4CAF50", }} />
-                  <View style={{ flex: 5, backgroundColor: "#E0E0E0" }} />
-                  <Icon size={24} color="black" name="keyboard-arrow-right" style={{marginLeft:'2%'}} />
+                  <View
+                    style={{
+                      flex: 1,
+                      flexDirection: "row",
+                      width: "100%",
+                      height: 20
+                    }}
+                  >
+                    <View style={{ flex: 4, backgroundColor: "#4CAF50" }} />
+                    <View style={{ flex: 5, backgroundColor: "#E0E0E0" }} />
+                    <Icon
+                      size={24}
+                      color="black"
+                      name="keyboard-arrow-right"
+                      style={{ marginLeft: "2%" }}
+                    />
                   </View>
-                  </View>
+                </View>
               </CardItem>
             </Card>
 
-             <Card>
-              <CardItem stlye={{
-                    width: width,
-                    height:50,
-                    padding: "5%",
-                    flex:1,
-                  }}>
-                  <View style={{flex:1,paddingBottom:'3%'}}>
-                  <View style={{flex:1,flexDirection:'column',width:'100%',height:20}}>
-                  <Text>Demo Course 4 (9 out of 10 complete)</Text>
+            <Card>
+              <CardItem
+                stlye={{
+                  width: width,
+                  height: 50,
+                  padding: "5%",
+                  flex: 1
+                }}
+              >
+                <View style={{ flex: 1, paddingBottom: "3%" }}>
+                  <View
+                    style={{
+                      flex: 1,
+                      flexDirection: "column",
+                      width: "100%",
+                      height: 20
+                    }}
+                  >
+                    <Text>Tensor Flow (9 out of 10 complete)</Text>
                   </View>
-                  <View style={{flex:1,flexDirection:'row',width:'100%',height:20}}>
-                  <View style={{ flex: 10, backgroundColor: "#4CAF50", }} />
-                  <View style={{ flex: 1, backgroundColor: "#E0E0E0" }} />
-                  <Icon size={24} color="black" name="keyboard-arrow-right" style={{marginLeft:'2%'}} />
+                  <View
+                    style={{
+                      flex: 1,
+                      flexDirection: "row",
+                      width: "100%",
+                      height: 20
+                    }}
+                  >
+                    <View style={{ flex: 10, backgroundColor: "#4CAF50" }} />
+                    <View style={{ flex: 1, backgroundColor: "#E0E0E0" }} />
+                    <Icon
+                      size={24}
+                      color="black"
+                      name="keyboard-arrow-right"
+                      style={{ marginLeft: "2%" }}
+                    />
                   </View>
-                  </View>
+                </View>
               </CardItem>
             </Card>
           </ScrollView>
